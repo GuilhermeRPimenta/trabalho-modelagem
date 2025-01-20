135764ddbc6d5a4cf8b310f05afbecd66f49d214
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2025-01-20 12:07:59 -0300

    Adds AnimalInfo component

--- client/src/pages/restricted/user/UserDonatedAnimal.tsx
+++ client/src/pages/restricted/user/UserDonatedAnimal.tsx
@@ -1,25 +1,13 @@
 import { useParams } from "react-router-dom";
 import { animals } from "../../../assets/exampleData";
-import { BsGenderFemale, BsGenderMale } from "react-icons/bs";
-import { useState } from "react";
-
-const calculateAge = (birthDate: Date) => {
-  const currentDate = new Date();
-  let years = currentDate.getFullYear() - birthDate.getFullYear();
-  let months = currentDate.getMonth() - birthDate.getMonth();
-  if (months < 0) {
-    years--;
-    months += 12;
-  }
-  return `${years} anos e ${months} meses`;
-};
+import AnimalInfo from "../../../components/global/AnimalInfo";
 
 const UserDonatedAnimal = () => {
   const { donatedAnimalId } = useParams<{ donatedAnimalId: string }>();
   const animal = animals.find(
     (animal) => animal.id === Number(donatedAnimalId)
   );
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
   if (!animal || !animal.adopter)
     return (
       <h1 className="font-dynapuff text-3xl text-blue-500">
@@ -28,104 +16,14 @@ const UserDonatedAnimal = () => {
     );
   return (
     <div className="flex flex-col w-full items-center gap-2">
-      <div className=" flex flex-col gap-2 items-center text-center p-4 rounded-sm w-full">
-        <div className="flex flex-col w-full xl:flex-row gap-5">
-          <div className="flex flex-col xl:basis-1/2 basis-auto gap-2">
-            <div className="flex justify-center">
-              <img
-                className="w-full shadow-lg  max-w-[30rem] aspect-square xl:w-[60vh] xl:h-[60vh] object-cover rounded-lg"
-                src={animal?.imgUrls[currentImageIndex]}
-                alt={`Imagem ${currentImageIndex + 1}`}
-              />
-            </div>
-            <div className="flex flex-wrap justify-center gap-2">
-              {animal.imgUrls.map((img, index) => {
-                return (
-                  <img
-                    key={img}
-                    src={img}
-                    alt={`Miniatura ${index + 1}`}
-                    className={`w-20 h-20 object-cover rounded-md cursor-pointer ${
-                      currentImageIndex === index
-                        ? "border-4 border-blue-500"
-                        : "border"
-                    }`}
-                    onClick={() => setCurrentImageIndex(index)}
-                  ></img>
-                );
-              })}
-            </div>
-          </div>
-
-          <div className="flex flex-col xl:basis-1/2 basis-auto justify-evenly gap-1 min-h-[60vh]">
-            <div className="flex flex-col gap-1">
-              <h1 className="font-semibold text-xl">{animal?.name}</h1>
-              <p>{animal.species ? animal.species : animal.customSpecies}</p>
-              <div className="flex  justify-center flex-row">
-                {animal?.gender === "MACHO" ? (
-                  <>
-                    <BsGenderMale size={24} />
-                    <p>Macho</p>
-                  </>
-                ) : (
-                  <>
-                    <BsGenderFemale size={24} />
-                    <p>Fêmea</p>
-                  </>
-                )}
-              </div>
-              <p>{animal?.description}</p>
-              {animal.breed && (
-                <>
-                  <h2 className="font-semibold">Raça: </h2>
-                  <p>{animal.breed}</p>
-                </>
-              )}
-              {animal.birthDate && (
-                <>
-                  <h2 className="font-semibold">Nascimento: </h2>
-                  <p>
-                    {animal.birthDate?.toLocaleString("pt-BR", {
-                      day: "2-digit",
-                      month: "2-digit",
-                      year: "2-digit",
-                    })}
-                  </p>
-                </>
-              )}
-              {(animal.birthDate || animal.age) && (
-                <>
-                  <h2 className="font-semibold">Idade: </h2>
-                  <p>
-                    {animal?.birthDate
-                      ? calculateAge(animal.birthDate)
-                      : animal?.age + " anos (no momento de registro)"}
-                  </p>
-                </>
-              )}
-              {animal.healthCondition && (
-                <>
-                  <h2 className="font-semibold">Condição de saúde: </h2>
-                  <p>{animal.healthCondition}</p>
-                </>
-              )}
-              {animal.weight && (
-                <>
-                  <h2 className="font-semibold">Peso: </h2>
-                  <p>{animal.weight} kg</p>
-                </>
-              )}
-            </div>
-
-            <div className="flex flex-col gap-1">
-              <h2 className="text-xl font-semibold">Adotante:</h2>
-              {animal.adopter.name}
-              <h3 className="font-semibold">Endereço</h3>
-              {`${animal.adopter.neighborhood}, ${animal.adopter.city} - ${animal.adopter.state}`}
-            </div>
-          </div>
+      <AnimalInfo animal={animal}>
+        <div className="flex flex-col gap-1">
+          <h2 className="text-xl font-semibold">Adotante:</h2>
+          {animal.adopter.name}
+          <h3 className="font-semibold">Endereço</h3>
+          {`${animal.adopter.neighborhood}, ${animal.adopter.city} - ${animal.adopter.state}`}
         </div>
-      </div>
+      </AnimalInfo>
     </div>
   );
 };
